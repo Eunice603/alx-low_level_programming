40237820851f3b80296580708d985d7dd148bd2d
eunice603 <euniceorina254@gmail.com>
2022-03-15 09:39:57 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-int main(void)
-{
-        putchar('');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
 
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{char *sh = "_putchar";
+        while (*sh)
+        {
+                _putchar(*sh);
+                sh++;
+        }
+        _putchar('\n');
         return (0);
 }
+
